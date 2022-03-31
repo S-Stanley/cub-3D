c9a42c42d2ae799e3385fb309a1bdc196a66f2a0
Antoine COUSINIE <acousini@e2r12p16.clusters.42paris.fr>
2022-03-31 19:46:08 +0200

    hooks #9

---
+++ srcs/hooks_handle.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks_handle.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acousini <acousini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 19:30:02 by acousini          #+#    #+#             */
+/*   Updated: 2022/03/31 19:37:36 by acousini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int	close_win_hook(t_game *game)
+{
+	// clean_struct_game(game, "success");
+	exit (0);
+	return (1);
+}
+
+int	key_press_hook(int keycode, t_game *game)
+{
+	if (keycode != KEY_DOWN || keycode != KEY_RIGHT
+		|| keycode != KEY_LEFT || keycode != KEY_UP)
+	{
+		moves(game, keycode);
+		draw_map_2d(game, -1, -1);
+	}
+	if (keycode == 65307)
+		close_win_hook(game);
+	return (0);
+}
