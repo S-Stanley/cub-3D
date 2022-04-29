64a23d7657ede7abdd65fdb14b046c24ce0bc321
Stanley SERBIN <stanleyserbin@gmail.com>
2022-04-29 19:44:23 +0200

    fix: protect again two player in the map #70

--- srcs/fixtures.c
+++ srcs/fixtures.c
@@ -6,7 +6,7 @@
 /*   By: stan <stan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 20:51:48 by stan              #+#    #+#             */
-/*   Updated: 2022/04/29 19:25:34 by stan             ###   ########.fr       */
+/*   Updated: 2022/04/29 19:43:46 by stan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,6 +116,35 @@ void	check_rgb(t_game game)
 	}
 }
 
+void	check_multiple_player(t_game game)
+{
+	int		i;
+	int		x;
+	int		player_found;
+
+	i = -1;
+	player_found = 0;
+	while (game.map[++i])
+	{
+		x = 0;
+		while (game.map[i][x])
+		{
+			if (game.map[i][x] == 'N'
+				|| game.map[i][x] == 'W'
+				|| game.map[i][x] == 'E'
+				|| game.map[i][x] == 'S')
+				player_found++;
+			x++;
+		}
+	}
+	if (player_found != 1)
+	{
+		free_game(game);
+		printf("Error\nCannot have no more or less than one player in the map\n");
+		exit(1);
+	}
+}
+
 t_game	init_game(char *filename)
 {
 	t_game		game;
@@ -135,5 +164,6 @@ t_game	init_game(char *filename)
 	game.player_dir = verif_player_exist(game);
 	verif_player_pos(&game);
 	check_rgb(game);
+	check_multiple_player(game);
 	return (game);
 }
