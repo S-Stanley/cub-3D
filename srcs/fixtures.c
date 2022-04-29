9d8dcae35a71a9b8c67094fe87dcd0308beeba96
Stanley SERBIN <stanleyserbin@gmail.com>
2022-04-29 22:23:11 +0200

    fix: check column closed map #70

--- srcs/fixtures.c
+++ srcs/fixtures.c
@@ -6,7 +6,7 @@
 /*   By: stan <stan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 20:51:48 by stan              #+#    #+#             */
-/*   Updated: 2022/04/29 19:43:46 by stan             ###   ########.fr       */
+/*   Updated: 2022/04/29 22:21:46 by stan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,6 +145,135 @@ void	check_multiple_player(t_game game)
 	}
 }
 
+void	check_line_closed_top(t_game game, int i, int x)
+{
+	if (game.map[i][x] == ' ')
+	{
+		if (game.map[i + 1][x] == '0' || game.map[i + 1][x - 1] != '1' || game.map[i + 1][x + 1] != '1')
+		{
+			printf("Error\nThe map is not closed at position %d %d\n", i, x);
+			free_game(game);
+			exit(1);
+		}
+		if (game.map[i + 1][x] == ' ')
+		{
+			check_line_closed_top(game, i + 1, x);
+		}
+		if (game.map[i + 1][x] == '1')
+			return ;
+	}
+}
+
+void	check_line_closed_bottom(t_game game, int i, int x)
+{
+	if (game.map[i][x] == ' ')
+	{
+		if (game.map[i - 1][x] == '0' || game.map[i - 1][x - 1] != '1' || game.map[i - 1][x + 1] != '1')
+		{
+			printf("Error\nThe map is not closed at position %d %d\n", i, x);
+			free_game(game);
+			exit(1);
+		}
+		if (game.map[i - 1][x] == ' ')
+		{
+			check_line_closed_bottom(game, i - 1, x);
+		}
+		if (game.map[i - 1][x] == '1')
+			return ;
+	}
+}
+
+void	check_line_closed_right(t_game game, int i, int x)
+{
+	if (game.map[i][x + 1] != 0)
+		return ;
+	if (game.map[i][x] == ' ')
+	{
+		if (game.map[i + 1][x] == '0' || game.map[i - 1][x] == '0')
+		{
+			printf("Error\nThe map is not closed at position %d %d\n", i, x);
+			free_game(game);
+			exit(1);
+		}
+		if (game.map[i][x - 1] == ' ')
+			check_line_closed_right(game, i, x - 1);
+		if (game.map[i][x - 1] == '1')
+			return ;
+	}
+}
+
+void	check_line_closed_left(t_game game, int i, int x)
+{
+	if (game.map[i][x] == ' ')
+	{
+		if (game.map[i + 1][x] == '0' || game.map[i - 1][x] == '0')
+		{
+			printf("Error\nThe map is not closed at position %d %d\n", i, x);
+			free_game(game);
+			exit(1);
+		}
+		if (game.map[i][x + 1] == ' ')
+			check_line_closed_left(game, i, x + 1);
+		if (game.map[i][x + 1] == '1')
+			return ;
+	}
+}
+
+void	verif_map_space(t_game game)
+{
+	int		i;
+	int		x;
+	int		start;
+
+	i = 0;
+	start = 0;
+	while (game.map[i])
+	{
+		x = 0;
+		if (game.map[i][x] == '1')
+			start++;
+		while (game.map[i][x])
+		{
+			if (i == 0 && start)
+				check_line_closed_top(game, i, x);
+			if (!game.map[i + 1])
+				check_line_closed_bottom(game, i, x);
+			check_line_closed_right(game, i, x);
+			if (x == 0 && start)
+				check_line_closed_left(game, i, x);
+			x++;
+		}
+		i++;
+	}
+}
+
+void	verif_column(t_game game)
+{
+	int		i;
+	int		x;
+	int		max;
+
+	i = 0;
+	max = 0;
+	while (game.map[i])
+	{
+		x = 0;
+		while (game.map[i][x])
+		{
+			if (x > max && game.map[i][x] != '1' && game.map[i][x] != ' ')
+			{
+				printf("Error\nThe map is not closed at position:%d %d\n", i, x);
+				free_game(game);
+				exit(1);
+			}
+			x++;
+		}
+		if (x > max)
+			max = x;
+		i++;
+	}
+}
+
 t_game	init_game(char *filename)
 {
 	t_game		game;
@@ -160,6 +289,8 @@ t_game	init_game(char *filename)
 	game = get_texture(game, filename);
 	verif_map_config(game);
 	verif_map_closed(game);
+	verif_map_space(game);
+	verif_column(game);
 	verif_player_exist(game);
 	game.player_dir = verif_player_exist(game);
 	verif_player_pos(&game);
