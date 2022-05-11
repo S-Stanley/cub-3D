31d6b5658f7ffb6509ae359dc108dad5fbc02674
Stanley SERBIN <sserbin@e2r11p14.clusters.42paris.fr>
2022-05-11 14:14:41 +0200

    fix #98

---
+++ srcs/check_recursive_unclose.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_recursive_unclose.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserbin <sserbin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 14:12:03 by sserbin           #+#    #+#             */
+/*   Updated: 2022/05/11 14:13:32 by sserbin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+void	recurise_map_unclose_right(t_game game, int i, int x)
+{
+	if ()
+}
+
+void	recurise_map_unclose(t_game game)
+{
+	recurise_map_unclose_right(game, 0, ft_strlen(x));
+	return ;
+}
